16ceded40f465c2effffc6b6c5db6f12486e1b3a
lerrybe <lerry597@gmail.com>
2022-03-22 22:18:14 +0900

    [feature] basename 추가

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
