92381f6e38f138eea855f6188428b2aa76c899ed
lerrybe <lerry597@gmail.com>
2022-03-22 20:47:33 +0900

    [feature] 프록시 설정 - 네이버, 유튜브

---
+++ src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/v1',
+    createProxyMiddleware({
+      target: 'https://openapi.naver.com/',
+      changeOrigin: true,
+    }),
+  );
+  app.use(
+    '/v3',
+    createProxyMiddleware({
+      target: 'https://www.googleapis.com/youtube/',
+      changeOrigin: true,
+    }),
+  );
+};
