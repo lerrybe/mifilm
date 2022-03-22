d17911dc1d12ee3b8b3bd1a871a820927fce3c52
lerrybe <lerry597@gmail.com>
2022-03-22 20:48:02 +0900

    [feature] App, index

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+// styles
+import { Reset } from 'styled-reset';
+import Container from './styles/Container';
+import './styles/css/set.css';
+
+// Components
+import BoxOfficePage from './pages/BoxOfficePage';
+import MainPage from './pages/MainPage';
+import MyOstPage from './pages/MyOstPage';
+import SearchFilmPage from './pages/SearchFilmPage';
+import ShowDetailFilmPage from './pages/ShowDetailFilmPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+export default function App() {
+  return (
+    <>
+      <Reset />
+      <Container className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/boxoffice" element={<BoxOfficePage />} />
+          <Route path="/search" element={<SearchFilmPage />} />
+          <Route path="/myost" element={<MyOstPage />} />
+          <Route path="/show/:movie" element={<ShowDetailFilmPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
